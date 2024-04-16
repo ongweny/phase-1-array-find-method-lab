4c6ed2b5ff32f8b983bc63c7c747e2a696730f1b
ongweny <ongwenyjoel@gmail.com>
2024-04-16 13:54:47 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(record){
+    const win = record.find(({result}) => result === "W");
+    if (win){
+        return win.year 
+    } 
+    else {
+        return undefined
+    }
+}
